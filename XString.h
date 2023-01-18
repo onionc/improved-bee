f6b1293ed524f267a203bbcbb5ada38f60309976
Chad <wsrfssx@yeah.net>
2023-01-18 17:52:23 +0800

    fix: 兼容linux,删掉不用的代码

--- XString.h
+++ XString.h
@@ -6,16 +6,11 @@
 #include <QString>
 #endif
 
-#include <windows.h>
 #include <string>
 using namespace std;
 
 namespace XString
 {
-    const char* wcharToChar(const wchar_t *wch);
-    const wchar_t* charToWChar(const char *ch);
-    void deleteCh(const char *ch);
-    void deleteCh(const wchar_t *wch);
 
     #ifdef QT
     QString charToQS(const char *ch);
