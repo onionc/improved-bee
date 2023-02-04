8ddccb325bed79597c0381ea8a68c59993c6a650
onion <wsrfssx@yeah.net>
2023-02-04 23:14:40 +0800

    fix: 绘图设置

--- plot/plot.cpp
+++ plot/plot.cpp
@@ -6,9 +6,64 @@ Plot::Plot(QWidget *parent) :
     ui(new Ui::Plot)
 {
     ui->setupUi(this);
+    // 标题
+    setWindowTitle("绘图");
+    // 主窗口关闭时，关闭子窗口
+    setAttribute(Qt::WA_QuitOnClose, false);
+
+    // ------------- 图形1 ------------------
+    // 增加线条
+    ui->mPlot1->addGraph();
+    ui->mPlot1->graph(0)->setPen(QPen(Qt::red));
+    ui->mPlot1->addGraph();
+    ui->mPlot1->graph(1)->setPen(QPen(Qt::blue));
+    ui->mPlot1->addGraph();
+    ui->mPlot1->graph(2)->setPen(QPen(Qt::gray));
+
+    // ------------- 图形2 ------------------
+    // 增加线条
+    ui->mPlot2->addGraph();
+    ui->mPlot2->graph(0)->setPen(QPen(Qt::red));
+    ui->mPlot2->addGraph();
+    ui->mPlot2->graph(1)->setPen(QPen(Qt::blue));
+    ui->mPlot2->addGraph();
+    ui->mPlot2->graph(2)->setPen(QPen(Qt::gray));
+
+    // ------------- 图形3 ------------------
+    // 增加线条
+    ui->mPlot3->addGraph();
+    ui->mPlot3->graph(0)->setPen(QPen(Qt::red));
+    ui->mPlot3->addGraph();
+    ui->mPlot3->graph(1)->setPen(QPen(Qt::blue));
+    ui->mPlot3->addGraph();
+    ui->mPlot3->graph(2)->setPen(QPen(Qt::gray));
 }
 
 Plot::~Plot()
 {
     delete ui;
+}
+
+
+void Plot::plotAddData(int index,
+                  const QVector<double> &key,
+                  const QVector<double> &value1,
+                  const QVector<double> &value2,
+                  const QVector<double> &value3){
+
+    if(index<1 || index>3){
+        return;
+    }
+
+    if(!value1.isEmpty()){
+        ui->mPlot1->graph(0)->addData(key, value1);
+    }
+
+    if(!value2.isEmpty()){
+        ui->mPlot1->graph(1)->addData(key, value2);
+    }
+
+    if(!value3.isEmpty()){
+        ui->mPlot1->graph(2)->addData(key, value3);
+    }
 }
--- plot/plot.h
+++ plot/plot.h
@@ -1,7 +1,11 @@
 #ifndef PLOT_H
 #define PLOT_H
 
+// 绘图组件，显示三组图形，每组三种数据
+
 #include <QWidget>
+#include <QPen>
+#include <QVector>
 
 namespace Ui {
 class Plot;
@@ -15,6 +19,20 @@ public:
     explicit Plot(QWidget *parent = 0);
     ~Plot();
 
+    //  index为图形索引
+    /**
+     * @brief plotAddData 图形增加数据
+     * @param index 图形索引，1~3 分别代表三个图形
+     * @param key x数据
+     * @param value1 y轴数据1
+     * @param value2 y轴数据2
+     * @param value3 z轴数据3
+     */
+    void plotAddData(int index,
+                      const QVector<double> &key,
+                      const QVector<double> &value1,
+                      const QVector<double> &value2,
+                      const QVector<double> &value3);
 private:
     Ui::Plot *ui;
 };
